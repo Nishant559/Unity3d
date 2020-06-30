1950a80c6b64c18548abc7d7189b30541dd73275
NISHANT KUMAR SINGH <55767210+Nishant559@users.noreply.github.com>
2020-06-30 23:41:42 +0530

    Add files via upload

---
+++ Scripts/AddConstantVelocity.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddConstantVelocity : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 v3Force;
+    // Start is called before the first frame update
+  
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().velocity += v3Force;
+    }
+}
